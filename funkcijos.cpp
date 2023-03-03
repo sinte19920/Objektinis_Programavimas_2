072a367413b6fd67d4442e7e156db301299e452a
sinte19920 <66333376+sinte19920@users.noreply.github.com>
2023-03-03 23:52:28 +0200

    Update funkcijos.cpp

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -1,88 +1,5 @@
 #include "funkcijos.h"
 
-// funkcija, skirta vidurkio arba medianos pasirinkimo nuskaitymui
-string read_average_type()
-{
-    string vidurkis_mediana;
-
-    cout << "Norite, kad galutiniame rezultate butu pateiktas rezultatu vidurkis ar mediana?" << endl
-         << "(irasykite 'med' - medianai, 'vid' - vidurkiui): ";
-
-    // nuskaitome vartotojo pasirinkimą, kol jis įves tinkamą reikšmę
-    while (vidurkis_mediana != "med" && vidurkis_mediana != "vid")
-        cin >> vidurkis_mediana;
-
-    return vidurkis_mediana;
-}
-
-// funkcija, apskaiciuojanti mediana arba vidurki
-void med_vid(int n, int nd, vector<Studentas> &studentai, string &vidurkis_mediana)
-{
-    vidurkis_mediana = read_average_type();
-    double vid = 0;
-    if (vidurkis_mediana == "vid")
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < nd; j++)
-                vid += studentai[i].pazymiai[j];
-            studentai[i].rezultatas = 0.4 * (vid / nd) + 0.6 * studentai[i].egz;
-            vid = 0;
-        }
-    }
-    else
-    {
-        for (int i = 0; i < n; i++)
-        {
-            int pazymiu_skaicius = nd;
-            // isrikiuojama nuo maziausio iki didziausio
-            sort(studentai[i].pazymiai.begin(), studentai[i].pazymiai.end());
-
-            if (pazymiu_skaicius % 2 == 0)
-                vid = (studentai[i].pazymiai[pazymiu_skaicius / 2 - 1] + studentai[i].pazymiai[pazymiu_skaicius / 2]) / 2;
-            else
-                vid = studentai[i].pazymiai[pazymiu_skaicius / 2];
-
-            studentai[i].rezultatas = 0.4 * (vid / nd) + 0.6 * studentai[i].egz;
-            vid = 0;
-        }
-    }
-}
-
-void rezultatu_spausdinimas(string vidurkis_mediana, vector<Studentas> &studentai)
-{
-    ofstream fr("rezultatai.txt");
-    fr << left << setw(20) << "Vardas"
-       << left << setw(20) << "Pavarde";
-    if (vidurkis_mediana == "vid")
-        fr << left << setw(15) << "Galutinis (Vid.)" << endl;
-    else
-        fr << left << setw(15) << "Galutinis (Med.)" << endl;
-
-    fr << "-----------------------------------------------" << endl;
-
-    int dydis = studentai.size();
-    for (int i = 0; i < dydis; i++)
-    {
-        fr << left << setw(20) << studentai[i].vardas
-           << left << setw(20) << studentai[i].pavarde
-           << left << setw(15) << fixed << setprecision(2) << studentai[i].rezultatas << endl;
-    }
-    fr.close();
-}
-
-// funkcija, palyginanti vardus
-bool pagal_varda(Studentas a, Studentas b)
-{
-    return a.vardas < b.vardas;
-}
-
-// funkcija, palyginanti pavardes
-bool pagal_pavarde(Studentas a, Studentas b)
-{
-    return a.pavarde < b.pavarde;
-}
-
 void duomenu_nuskaitymas(vector<Studentas> &studentai)
 {
     string filename;
@@ -151,7 +68,110 @@ void duomenu_nuskaitymas(vector<Studentas> &studentai)
     while (atsakymas != "vard" && atsakymas != "pavard")
         cin >> atsakymas;
 
-    rezultatu_spausdinimas(vidurkis_mediana, studentai, studentu_sk);
+    rezultatu_spausdinimas(vidurkis_mediana, studentai);
 
     fd.close();
+}
+// funkcija, skirta vidurkio arba medianos pasirinkimo nuskaitymui
+string read_average_type()
+{
+    string vidurkis_mediana;
+
+    cout << "Norite, kad galutiniame rezultate butu pateiktas rezultatu vidurkis ar mediana?" << endl
+         << "(irasykite 'med' - medianai, 'vid' - vidurkiui): ";
+
+    do
+    {
+        try
+        {
+            cin >> vidurkis_mediana;
+            if (vidurkis_mediana != "med" && vidurkis_mediana != "vid")
+            {
+                throw invalid_argument("Klaida: ivestas neteisingas simbolis.");
+            }
+            else
+            {
+                break;
+            }
+        }
+        catch (const invalid_argument &e)
+        {
+            cout << e.what() << endl;
+            cin.clear();
+            cin.ignore();
+        }
+    } while (true);
+
+    // nuskaitome vartotojo pasirinkimą, kol jis įves tinkamą reikšmę
+    // while (vidurkis_mediana != "med" && vidurkis_mediana != "vid")
+
+    return vidurkis_mediana;
+}
+
+// funkcija, apskaiciuojanti mediana arba vidurki
+void med_vid(int n, int nd, vector<Studentas> &studentai, string &vidurkis_mediana)
+{
+    vidurkis_mediana = read_average_type();
+    double vid = 0;
+    if (vidurkis_mediana == "vid")
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < nd; j++)
+                vid += studentai[i].pazymiai[j];
+            studentai[i].rezultatas = 0.4 * (vid / nd) + 0.6 * studentai[i].egz;
+            vid = 0;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int pazymiu_skaicius = nd;
+            // isrikiuojama nuo maziausio iki didziausio
+            sort(studentai[i].pazymiai.begin(), studentai[i].pazymiai.end());
+
+            if (pazymiu_skaicius % 2 == 0)
+                vid = (studentai[i].pazymiai[pazymiu_skaicius / 2 - 1] + studentai[i].pazymiai[pazymiu_skaicius / 2]) / 2;
+            else
+                vid = studentai[i].pazymiai[pazymiu_skaicius / 2];
+
+            studentai[i].rezultatas = 0.4 * (vid / nd) + 0.6 * studentai[i].egz;
+            vid = 0;
+        }
+    }
+}
+
+// funkcija, palyginanti vardus
+bool pagal_varda(Studentas a, Studentas b)
+{
+    return a.vardas < b.vardas;
+}
+
+// funkcija, palyginanti pavardes
+bool pagal_pavarde(Studentas a, Studentas b)
+{
+    return a.pavarde < b.pavarde;
+}
+
+void rezultatu_spausdinimas(string vidurkis_mediana, vector<Studentas> &studentai)
+{
+    ofstream fr("rezultatai.txt");
+    fr << left << setw(20) << "Vardas"
+       << left << setw(20) << "Pavarde";
+    if (vidurkis_mediana == "vid")
+        fr << left << setw(15) << "Galutinis (Vid.)" << endl;
+    else
+        fr << left << setw(15) << "Galutinis (Med.)" << endl;
+
+    fr << "-----------------------------------------------" << endl;
+
+    int dydis = studentai.size();
+    for (int i = 0; i < dydis; i++)
+    {
+        fr << left << setw(20) << studentai[i].vardas
+           << left << setw(20) << studentai[i].pavarde
+           << left << setw(15) << fixed << setprecision(2) << studentai[i].rezultatas << endl;
+    }
+    fr.close();
 }
