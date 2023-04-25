8dd25547e62a0534e5465f5d14f6f74758263ab2
sinte19920 <66333376+sinte19920@users.noreply.github.com>
2023-04-25 21:47:34 +0300

    Update main_list.cpp

--- main_list.cpp
+++ main_list.cpp
@@ -7,13 +7,13 @@ LIST
 
 int main()
 {
-    //auto start = std::chrono::high_resolution_clock::now(); // paleisti
+    auto start = std::chrono::high_resolution_clock::now(); // paleisti
 
     list<Studentas> studentai;
     duomenu_nuskaitymas_list(studentai);
 
-    //auto end = std::chrono::high_resolution_clock::now(); // stabdyti
-    //std::chrono::duration<double> skirt = end - start;    // skirtumas (s)
-    //cout << "Programos veiklos sparta: " << fixed << setprecision(2) << skirt.count() << "s" << endl;
+    auto end = std::chrono::high_resolution_clock::now(); // stabdyti
+    std::chrono::duration<double> skirt = end - start;    // skirtumas (s)
+    cout << "Programos veiklos sparta: " << fixed << setprecision(2) << skirt.count() << "s" << endl;
     return 0;
 }
