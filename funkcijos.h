b7acce139144d1efc47ca6cbc38cf2dcfff163c0
sinte19920 <66333376+sinte19920@users.noreply.github.com>
2023-04-25 21:36:08 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -18,13 +18,13 @@ string generate_surname(int);
 int generate_grade();
 string read_average_type();
 bool compare(Studentas, Studentas);
-void med_vid(int, int, vector<Studentas> &, string &);
+void med_vid(int, int, vector<Studentas> &, string &, int);
 void duomenu_nuskaitymas(vector<Studentas> &);
 
 // List
-void med_vid_list(int n, int nd, list<Studentas> &studentai, string &vidurkis_mediana, list<Studentas> &neislaike, list<Studentas> &islaike);
+void med_vid_list(int n, int nd, list<Studentas> &studentai, string &vidurkis_mediana, list<Studentas> &neislaike, list<Studentas> &islaike, int strategija);
 void duomenu_nuskaitymas_list(list<Studentas> &);
 
 // Deque
-void med_vid_deq(int n, int nd, deque<Studentas> &studentai, string &vidurkis_mediana, deque<Studentas> &neislaike, deque<Studentas> &islaike);
+void med_vid_deq(int n, int nd, deque<Studentas> &studentai, string &vidurkis_mediana, deque<Studentas> &neislaike, deque<Studentas> &islaike, int strategija);
 void duomenu_nuskaitymas_deq(deque<Studentas> &);
