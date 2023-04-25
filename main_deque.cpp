9b330624eceeb1ebe2520410e4cec4cbcacc7a00
sinte19920 <66333376+sinte19920@users.noreply.github.com>
2023-04-26 00:28:28 +0300

    Add files via upload

---
+++ main_deque.cpp
@@ -0,0 +1,19 @@
+/*
+DEQUE
+*/
+
+#include "Mylib.h"
+#include "funkcijos.h"
+
+int main()
+{
+    //auto start = std::chrono::high_resolution_clock::now(); // paleisti
+
+    deque<Studentas> studentai;
+    duomenu_nuskaitymas_deq(studentai);
+
+    //auto end = std::chrono::high_resolution_clock::now(); // stabdyti
+    //std::chrono::duration<double> skirt = end - start;    // skirtumas (s)
+    //cout << "Programos veiklos sparta: " << fixed << setprecision(2) << skirt.count() << "s" << endl;
+    return 0;
+}
