301bd14ca32cd24e7e0e1a9b8d773b1114173f8f
sinte19920 <66333376+sinte19920@users.noreply.github.com>
2023-04-25 21:46:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ VECTOR
 
 int main()
 {
-    //auto start = std::chrono::high_resolution_clock::now(); // paleisti
+    auto start = std::chrono::high_resolution_clock::now(); // paleisti
 
     vector<Studentas> studentai;
     duomenu_nuskaitymas(studentai);
 
-    //auto end = std::chrono::high_resolution_clock::now(); // stabdyti
-    //std::chrono::duration<double> skirt = end - start;    // skirtumas (s)
-    //cout << "Programos veiklos sparta: " << fixed << setprecision(2) << skirt.count() << "s" << endl;
+    auto end = std::chrono::high_resolution_clock::now(); // stabdyti
+    std::chrono::duration<double> skirt = end - start;    // skirtumas (s)
+    cout << "Programos veiklos sparta: " << fixed << setprecision(2) << skirt.count() << "s" << endl;
     return 0;
 }
