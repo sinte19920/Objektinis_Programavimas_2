8518f639db30c7b5134452bdfc2c3d677e070527
sinte19920 <66333376+sinte19920@users.noreply.github.com>
2023-05-29 18:32:50 +0300

    Add files via upload

---
+++ html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "svz",
+  1: "sz",
+  2: "v"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Pages"
+};
+
